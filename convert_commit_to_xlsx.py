a35c925a79aa4711734ad75684ce258bb9c1977f
pryntik <alexandre.lemetteil@gmail.com>
2025-05-19 01:29:14 +0200

    Support for merge commits
    - Adding a gitignore and an assets folder
    - Managing input and output files in the assets folder

--- convert_commit_to_xlsx.py
+++ convert_commit_to_xlsx.py
@@ -5,26 +5,24 @@ import pandas as pd
 from io import StringIO
 from datetime import datetime
 
-# File Name
-file_name = 'commits'
+# Assets
+root_dir = os.path.dirname(os.path.abspath(__file__))
+asset_dir = os.path.join(root_dir, 'assets')
+os.makedirs(asset_dir, exist_ok=True)
 
-# Content exemple
-commit_text = """
-* commit id_commit_1
-| Author: FirstName LastName <name@example.com>
-| Date:   Mon Jan 01 01:01:01 2000 +0200
-|
-|     Title of the commit 1
-|
-* commit id_commit_2
-| Author: FirstName LastName <name@example.com>
-| Date:   Mon Feb 01 01:01:01 2000 +0200
-|
-|     Title of the commit 2
-|
-|     Description line 1 of the commit.
-|     Description line 2 of the commit.
-"""
+# Files
+file_name = 'commits'
+file_input = f'{file_name}.txt'
+file_output = f'{file_name}.xlsx'
+
+# Paths
+path_input = os.path.join(asset_dir, file_input)
+path_output =  os.path.join(asset_dir, file_output)
+
+# Console command in input file.txt :
+# git log --all --decorate --graph
+with open(path_input, 'r', encoding='utf-8') as f:
+    commit_text = f.read()
 
 def month_short_to_full(short_month):
     months = {
@@ -44,7 +42,7 @@ def month_short_to_full(short_month):
     return months.get(short_month, short_month)
 
 # Cutting of Committee blocks
-commit_blocks = re.split(r'\* commit ', commit_text)[1:]
+commit_blocks = re.split(r"^\s*[\s|/\\]*\*\s*[\s|/\\]*commit\s+", commit_text, flags=re.MULTILINE)[1:]
 
 # Store the commits grouped by year-house
 grouped_commits = {}
@@ -52,10 +50,13 @@ grouped_commits = {}
 for block in commit_blocks:
     # Clean : strip and '|'
     block_lines = block.strip().split('\n')
-    lines = [
-        line.strip().lstrip('|').strip() for line in block_lines
-        if line.strip() != '|'
-    ]
+    processed_lines = []
+    for current_line in block_lines:
+        # Remove all '|', '\', '/' at start and strip the line
+        cleaned_line = re.sub(r"^[|\s/\\]*", "", current_line).strip()
+        if cleaned_line:
+            processed_lines.append(cleaned_line)
+    lines = processed_lines
 
     commit_id = lines[0] if lines else ''
 
@@ -112,23 +113,17 @@ for key in sorted(grouped_commits.keys()):
             'Author': commit['Author']
         })
 
-# Creation of dataframe
-df = pd.DataFrame(rows)
-
-# Get the path of the script file
-script_dir = os.path.dirname(os.path.abspath(__file__))
-output_path = os.path.join(script_dir, f'{file_name}.xlsx')
-
 # Backup in an xlsx file
-df.to_excel(output_path, index=False)
+df = pd.DataFrame(rows)
+df.to_excel(path_output, index=False)
 
 # Adjust the width of each column according to the maximum content
-wb = openpyxl.load_workbook(output_path)
+wb = openpyxl.load_workbook(path_output)
 ws = wb.active
 for column_cells in ws.columns:
     length = max(len(str(cell.value)) if cell.value else 0 for cell in column_cells)
     ws.column_dimensions[column_cells[0].column_letter].width = length
 
 # Back up the modified file
-wb.save(output_path)
-print(f"file xlsx generated successfully at the location : {output_path}")
+wb.save(path_output)
+print(f"file xlsx generated successfully at the location : {path_output}")
